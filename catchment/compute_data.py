5eedf7a5a7a2632147ee6264fc73894a5874f619
HannahRYoung <hanyou@ceh.ac.uk>
2024-03-20 14:35:04 +0000

    decoupling analyse_data function and test

--- catchment/compute_data.py
+++ catchment/compute_data.py
@@ -15,16 +15,26 @@ def analyse_data(data_dir):
     works out the mean for each day, and then graphs the standard deviation
     of these means.
     """
+    data = load_catchment_data(data_dir)
+    daily_standard_deviation = compute_standard_deviation_by_day(data)
+
+    return daily_standard_deviation
+
+
+def daily_std(dataset):
+    daily_std = dataset.groupby(dataset.index.date).std()
+    return daily_std
+
+
+def compute_standard_deviation_by_day(data):
+    daily_std_list = map(daily_std,data)
+    daily_standard_deviation = pd.concat(daily_std_list)
+    return daily_standard_deviation
+
+
+def load_catchment_data(data_dir):
     data_file_paths = glob.glob(os.path.join(data_dir, 'rain_data_2015*.csv'))
     if len(data_file_paths) == 0:
         raise ValueError('No CSV files found in the data directory')
     data = map(models.read_variable_from_csv, data_file_paths)
-
-    daily_std_list = []
-    for dataset in data:
-        daily_std = dataset.groupby(dataset.index.date).std()
-        daily_std_list.append(daily_std)
-    
-    daily_standard_deviation = pd.concat(daily_std_list)
-
-    return daily_standard_deviation
+    return data
