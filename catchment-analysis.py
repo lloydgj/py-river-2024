3b9a37af75ae52abfabfe5371b824027774d3311
Alex <alexander.riddell@manchester.ac.uk>
2024-03-21 10:09:49 +0000

    Argparse in seperate function

--- catchment-analysis.py
+++ catchment-analysis.py
@@ -21,7 +21,7 @@ def main(args):
     if args.full_data_analysis:
         _, extension = os.path.splitext(InFiles[0])
         if extension =='.json':
-            print("running .jason")
+            print("running .json")
             data_source = compute_data.JSONDataSource(os.path.dirname(InFiles[0]))
         elif extension =='.csv':
             print("running .csv")
@@ -34,9 +34,9 @@ def main(args):
 
         graph_data = {
         'daily standard deviation': daily_standard_deviation
-    }
+        }
 
-    views.visualize(graph_data)
+        views.visualize(graph_data)
 
 
     for filename in InFiles:
@@ -45,18 +45,24 @@ def main(args):
         view_data = {'daily sum': models.daily_total(measurement_data), 'daily average': models.daily_mean(measurement_data), 'daily max': models.daily_max(measurement_data), 'daily min': models.daily_min(measurement_data)}
         
         views.visualize(view_data)
-
-if __name__ == "__main__":
+def create_argparse():
     parser = argparse.ArgumentParser(
         description='A basic environmental data management system')
-    
+     
     parser.add_argument(
         'infiles',
         nargs='+',
         help='Input CSV(s) containing measurement data')
 
-    parser.add_argument('--full-data-analysis', action='store_true', dest='full_data_analysis')
-    
+    parser.add_argument('--full_data_analysis', action='store_true', dest='full_data_analysis')
+
+    return parser
+
+if __name__ == "__main__":
+     
+     
+    parser = create_argparse()
+
     args = parser.parse_args()
     
     main(args)
