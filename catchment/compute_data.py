dd69d16d18a5b5c1acfb64e57a86f2a8d6a15d8a
HannahRYoung <hanyou@ceh.ac.uk>
2024-03-22 13:54:19 +0000

    added docstrings in compute_data

--- catchment/compute_data.py
+++ catchment/compute_data.py
@@ -23,17 +23,23 @@ def analyse_data(data_dir):
 
 
 def daily_std(dataset):
+    """Calculate the daily standard deviation of a 2D data array.
+    Index must be np.datetime64 compatible format."""
     daily_std = dataset.groupby(dataset.index.date).std()
     return daily_std
 
 
 def compute_standard_deviation_by_day(data):
+    """Calculate the standard deviation by day of a set of datasets."""
     daily_std_list = map(daily_std,data)
     daily_standard_deviation = pd.concat(daily_std_list)
     return daily_standard_deviation
 
 
 def load_catchment_data(data_dir):
+    """Load in Catchment data CSV files called rain_data_2015*.csv 
+    from the data directory.
+    """
     data_file_paths = glob.glob(os.path.join(data_dir, 'rain_data_2015*.csv'))
     if len(data_file_paths) == 0:
         raise ValueError('No CSV files found in the data directory')
@@ -41,6 +47,8 @@ def load_catchment_data(data_dir):
     return data
 
 class CSVDataSource:
+    """Creates a class for CSV data sources.
+    Function load_catchment_data allows for loading in the CSV files."""
     def __init__(self, data_dir):
         self.data_dir = data_dir
     def load_catchment_data(self):
@@ -51,6 +59,8 @@ class CSVDataSource:
         return data
     
 class JSONDataSource:
+    """Creates a class for JSON data sources.
+    Function load_catchment_data allows for loading in the JSON files."""
     def __init__(self, data_dir):
         self.data_dir = data_dir
     def load_catchment_data(self):
